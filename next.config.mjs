e2dc06b3b8bb0deef6a5754b8e70175849eabf02
DanamiStartrail <mandantagurusinga@gmail.com>
2026-04-19 00:01:36 +0700

    Fix Final Deploy Issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,30 @@
 import withPWAInit from '@ducanh2912/next-pwa';
 
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Pengaman agar build tidak gagal karena error library luar
+  // Membungkam error Turbopack di Next 16
+  turbopack: {}, 
+
+  // Skip pengecekan error saat build biar lancar
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: '**.supabase.co',
+        pathname: '/**', // Mengizinkan semua path gambar dari Supabase
       },
     ],
   },
 };
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
 
 export default withPWA(nextConfig);
